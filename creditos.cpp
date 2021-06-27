b0667c068834c0c2aac4707bd7fd4eace2c737a3
Natalids <67949720+Natalids@users.noreply.github.com>
2021-06-28 00:12:06 +0200

    Create creditos.cpp

---
+++ creditos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <clocale>
+using namespace std;
+
+int main(void){
+  setlocale(LC_ALL, "Spanish");
+
+  cout << "EQUIPO 'NIDEAH'" << endl << endl;
+  cout << "LEGAJO          APELLIDO Y NOMBRE" << endl;
+  cout << " 24960          Della Savia Natalí" << endl;
+  cout << " 25064          Taboada Lautaro" << endl;
+  
+	return 0;
+}
