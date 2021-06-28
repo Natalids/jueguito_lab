ac01cfa01ee0100dbfde5c9bbffd20877aa69085
Natalids <67949720+Natalids@users.noreply.github.com>
2021-06-28 16:07:22 +0200

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,1 +1,10 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
 
+// Declaración de las funciones
+
+void menu(void);
+void creditos(void);
+void estadisticas(int, char);
+
+#endif // FUNCIONES_H_INCLUDED
