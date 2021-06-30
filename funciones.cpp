e9d7087e82b9c5ccee53ab9deb3c78bf0b4b2ba7
Natalids <67949720+Natalids@users.noreply.github.com>
2021-06-30 10:00:57 +0200

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -3,31 +3,31 @@
 using namespace std;
 #include "funciones.h" // Nuestro archivo de funciones
 #include <cstring> // Para laburar/comparar las cadenas de texto
-#include <ctime> // Es la librería del 'tiempo'. Sirve para disponer del dato de la fecha y hora para generar un primer número aleatorio. Se va a usar para los números de la matriz
-#include "rlutil.h" // La librería para poner bonita la consola
+#include <ctime> // Es la librerÃ­a del 'tiempo'. Sirve para disponer del dato de la fecha y hora para generar un primer nÃºmero aleatorio. Se va a usar para los nÃºmeros de la matriz
+#include "rlutil.h" // La librerÃ­a para poner bonita la consola
 
 void menu(void){
 	cout << "  MATHRIX" << endl;
 	cout << "  ---------------------" << endl;
 	cout << "  1 - JUGAR" << endl;
-	cout << "  2 - ESTADÍSTICAS" << endl;
-	cout << "  3 - CRÉDITOS" << endl;
+	cout << "  2 - ESTADÃSTICAS" << endl;
+	cout << "  3 - CRÃ‰DITOS" << endl;
 	cout << "  ---------------------" << endl;
 	cout << "  0 - SALIR" << endl << endl;
 }
 
 void creditos(void){
-    cout << endl << "  CRÉDITOS" << endl;
+    cout << endl << "  CRÃ‰DITOS" << endl;
 	cout << "  ---------------------------------" << endl;
     cout << "  EQUIPO 'LANA'" << endl << endl;
     cout << "  LEGAJO         APELLIDO Y NOMBRE" << endl;
-    cout << "  24960          Della Savia Natalí" << endl;
+    cout << "  24960          Della Savia NatalÃ­" << endl;
     cout << "  25064          Taboada Lautaro" << endl;
 	cout << "  ---------------------------------" << endl;
-    cout << endl << "  Apretá alguna tecla para volver al menú principal";
+    cout << endl << "  ApretÃ¡ alguna tecla para volver al menÃº principal";
 }
 
-void estadisticas(int puntaje, int vueltaUno, char const *nombre){ // El * es porque es por referencia
+void estadisticas(int puntaje, int *vueltaUno, char const *nombre){ // El * es porque es por referencia
     int mayorPuntaje;
     char mayorNombre[20]={"-"};
 
@@ -42,14 +42,37 @@ void estadisticas(int puntaje, int vueltaUno, char const *nombre){ // El * es po
         }
     }
 
-    cout << endl << "  ESTADÍSTICAS" << endl;
+    cout << endl << "  ESTADÃSTICAS" << endl;
 	cout << "  -------------------------------" << endl;
     cout << "  MAYOR PUNTAJE        NOMBRE" << endl;
     cout << "  " << mayorPuntaje << "                  " << mayorNombre << endl;
 	cout << "  ----------------------------------" << endl;
-    cout << endl << "  Apretá alguna tecla para volver al menú principal";
+    cout << endl << "  ApretÃ¡ alguna tecla para volver al menÃº principal";
 }
 
 void datos(char const *jugador, char const *calculadora, int pilas, int ronda, int puntaje){ // Barra de arriba de todo del juego
-    cout << endl << "  JUGADOR/A: " << jugador << "  |  CALCULADORA: " << calculadora << "  |  PILAS: " << pilas << "  |  Nº RONDA: " << ronda << "  |  PUNTAJE: " << puntaje;
+    cout << endl << "  JUGADOR/A: " << jugador << "  |  CALCULADORA: " << calculadora << "  |  PILAS: " << pilas << "  |  NÂº RONDA: " << ronda << "  |  PUNTAJE: " << puntaje;
+}
+
+void matrizAleatoria(int matriz[][6], int tam) {
+    int i, x;
+    cout << endl << endl;
+    for (int i=0; i<6; i++){
+        for (int x=0; x<tam; x++) {
+            matriz[i][x] = rand()%9+1;
+            cout << matriz[i][x] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void imprimirMatriz(int matriz[][6], int tam) {
+    int i, x;
+    cout << endl << endl;
+    for (int i=0; i<6; i++){
+        for (int x=0; x<tam; x++) {
+            cout << matriz[i][x] << " ";
+        }
+        cout << endl;
+    }
 }
