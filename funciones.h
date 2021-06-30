5826644207b8462cf222857cc7969ecca46304bd
Natalids <67949720+Natalids@users.noreply.github.com>
2021-06-30 10:01:10 +0200

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,13 +1,16 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
-// Declaración de las funciones
+// DeclaraciÃ³n de las funciones
 
 void menu(void);
 void creditos(void);
-void estadisticas(int, int, char const *nombre); // El * es porque es por referencia
+void estadisticas(int, int *, char const *nombre); // El * es porque es por referencia
 int jugar(int *, char *); // El * es porque es por referencia
 void datos(char const *, char const *, int, int, int);
+void matrizAleatoria(int [][6], int);
+void imprimirMatriz(int [][6], int);
+
 
 #endif // FUNCIONES_H_INCLUDED
 
