fefe6f71b5ef82b3d93308844ecb72c1ab090f39
Natalids <67949720+Natalids@users.noreply.github.com>
2021-07-02 18:54:22 +0200

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <clocale>
 using namespace std;
-#include "funciones.h" // Nuestro archivo de funciones
-#include <cstring> // Para laburar/comparar las cadenas de texto
-#include <ctime> // Es la librería del 'tiempo'. Sirve para disponer del dato de la fecha y hora para generar un primer número aleatorio. Se va a usar para los números de la matriz
-#include "rlutil.h" // La librería para poner bonita la consola
+#include "funciones.h"
+#include <cstring>
+#include <ctime>
+#include "rlutil.h"
 
 void menu(void){
 	cout << "  MATHRIX" << endl;
@@ -27,7 +27,7 @@ void creditos(void){
     cout << endl << "  Apretá alguna tecla para volver al menú principal";
 }
 
-void estadisticas(int puntaje, int *vueltaUno, char const *nombre){ // El * es porque es por referencia
+void estadisticas(int puntaje, int vueltaUno, char const *nombre){
     int mayorPuntaje;
     char mayorNombre[20]={"-"};
 
@@ -50,29 +50,186 @@ void estadisticas(int puntaje, int *vueltaUno, char const *nombre){ // El * es p
     cout << endl << "  Apretá alguna tecla para volver al menú principal";
 }
 
-void datos(char const *jugador, char const *calculadora, int pilas, int ronda, int puntaje){ // Barra de arriba de todo del juego
+void datos(char const *jugador, char const *calculadora, int pilas, int ronda, int puntaje){ // Barra de arriba de todo de 'jugar'.
     cout << endl << "  JUGADOR/A: " << jugador << "  |  CALCULADORA: " << calculadora << "  |  PILAS: " << pilas << "  |  Nº RONDA: " << ronda << "  |  PUNTAJE: " << puntaje;
 }
 
-void matrizAleatoria(int matriz[][6], int tam) {
+void matrizAleatoria(int matriz[][6], int tam){
+    int i, x;
+    for (i=0; i<6; i++){
+        for (x=0; x<6; x++){
+            matriz[i][x] = rand()%9+1;
+        }
+    }
+}
+
+void imprimirMatriz(int matriz[][6], int tam){
     int i, x;
     cout << endl << endl;
-    for (int i=0; i<6; i++){
-        for (int x=0; x<tam; x++) {
-            matriz[i][x] = rand()%9+1;
-            cout << matriz[i][x] << " ";
+    for (int i=0; i<tam; i++){
+        for(int x=0; x<tam; x++){
+            if(matriz[i][x]==-1){
+                cout << matriz[i][x] << " ";
+            }
+            else{
+                cout << " " << matriz[i][x] << " ";
+            }
         }
         cout << endl;
     }
 }
 
-void imprimirMatriz(int matriz[][6], int tam) {
-    int i, x;
-    cout << endl << endl;
-    for (int i=0; i<6; i++){
-        for (int x=0; x<tam; x++) {
-            cout << matriz[i][x] << " ";
+int ingresoDatos(int *fila, int *columna, char *operador, int *direccion){
+    cout << endl << "  Ingresá los valores correspondientes: ";
+    cout << endl << "  Nº FILA: ";
+    cin >> *fila;
+    cout << "  Nº COLUMNA: ";
+    cin >> *columna;
+    *fila = *fila-1;
+    *columna = *columna-1;
+    cout << "  Operación: (+) SUMAR | (-) RESTAR | (/) DIVIDIR | (*) MULTIPLICAR | (%) RESTO: ";
+    cin >> *operador;
+    cout << "  Dirección: (8) ARRIBA | (2) ABAJO  | (4) IZQUIERDA | (6) DERECHA: ";
+    cin >> *direccion;
+}
+
+int direcciones(int matriz [][6], int tam, int direccion, int fila, int columna, int *primerNumero, int *segundoNumero, int *tercerNumero){
+    *primerNumero = matriz[fila][columna];
+
+    if(direccion == 8){ // ARRIBA
+        *segundoNumero = matriz[fila-1][columna];
+        *tercerNumero = matriz[fila-2][columna];
+    }
+    else{
+        if(direccion == 2){ // ABAJO
+            *segundoNumero = matriz[fila+1][columna];
+            *tercerNumero = matriz[fila+2][columna];
         }
-        cout << endl;
+        else{
+            if(direccion == 4){ // IZQUIERDA
+                *segundoNumero = matriz[fila][columna-1];
+                *tercerNumero = matriz[fila][columna-2];
+            }
+            else{
+                if (direccion == 6){ // DERECHA
+                    *segundoNumero = matriz[fila][columna+1];
+                    *tercerNumero = matriz[fila][columna+2];
+                }
+            }
+        }
+    }
+}
+
+int ecuacion(char operador, int primerNumero, int segundoNumero, int tercerNumero, int *acumular){
+    int resultado;
+
+    if(operador == '+'){
+        resultado = primerNumero + segundoNumero;
+    }
+    else{
+        if(operador == '-'){
+            resultado = primerNumero - segundoNumero;
+        }
+        else{
+            if(operador == '*') {
+                resultado = primerNumero * segundoNumero;
+            }
+            else{
+                if(operador == '/'){
+                    resultado = primerNumero / segundoNumero;
+                }
+                else{
+                    if(operador == '%'){
+                        resultado = primerNumero % segundoNumero;
+                    }
+                }
+            }
+        }
+    }
+
+    if(resultado==tercerNumero){
+        *acumular = primerNumero + segundoNumero + tercerNumero;
+    }
+    else{
+        *acumular = 0;
+    }
+}
+
+int cambioCeldas(int matriz [][6], int tam, int fila, int columna, int direccion){
+    if(direccion == 8){ // ARRIBA
+        matriz[fila][columna] = -1;
+        matriz[fila-1][columna] = -1;
+        matriz[fila-2][columna] = -1;
+    }
+    else{
+        if(direccion == 2){ // ABAJO
+            matriz[fila][columna] = -1;
+            matriz[fila+1][columna] = -1;
+            matriz[fila+2][columna] = -1;
+        }
+        else{
+            if(direccion == 4){ // IZQUIERDA
+                matriz[fila][columna] = -1;
+                matriz[fila][columna-1] = -1;
+                matriz[fila][columna-2] = -1;
+            }
+            else{
+                if(direccion == 6){ // DERECHA
+                    matriz[fila][columna] = -1;
+                    matriz[fila][columna+1] = -1;
+                    matriz[fila][columna+2] = -1;
+                }
+            }
+        }
+    }
+}
+
+int salirseMatriz(int fila, int columna, int direccion, int *afuera){
+    if(fila==1 && direccion==8 || fila==2 && direccion==8){
+        *afuera = 1;
+    }
+    else{
+        if(fila==5 && direccion==2 || fila==6 && direccion==2){
+            *afuera = 1;
+        }
+        else{
+            if(columna==1 && direccion==4 || columna==2 && direccion==4){
+                *afuera = 1;
+            }
+            else{
+                if(columna==5 && direccion==6 || columna==6 && direccion==6){
+                    *afuera = 1;
+                }
+            }
+        }
+    }
+}
+
+int celdaNegativo(int matriz [][6], int tam, int fila, int columna, int direccion, int *negativo){
+    if(direccion == 8){
+        if(matriz[fila][columna] = -1 || matriz[fila-1][columna] = -1 || matriz[fila-2][columna] = -1){
+           *negativo = 1;
+        }
+    }
+    else{
+        if(direccion == 2){
+            if(matriz[fila][columna] = -1 || matriz[fila+1][columna] = -1 || matriz[fila+2][columna] = -1){
+            *negativo = 1;
+            }
+        }
+        else{
+            if(direccion == 4){
+                if(matriz[fila][columna] = -1 ||matriz[fila][columna-1] = -1 || matriz[fila][columna-2] = -1){
+                    *negativo = 1;
+                }
+            }
+            else{
+                if(direccion == 6){
+                    if(matriz[fila][columna] = -1 || matriz[fila][columna+1] = -1 || matriz[fila][columna+2] = -1){
+                        *negativo = 1;
+                    }
+                }
+            }
+        }
     }
 }
